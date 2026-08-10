fe1e205c91567cbbeb7740d352918c2d891035f4
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-08-10 11:34:22 +0600

    add all route in app.jsx page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,61 @@
 
 
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
+import {Toaster} from 'react-hot-toast'
+import LandingPage from './pages/LandingPage/LandingPage';
+import SignUp from './pages/Auth/SignUp';
+import Login from './pages/Auth/Login';
+import JobSeekerDashboard from './pages/JobSeeker/JobSeekerDashboard';
+import JobDetails from './pages/JobSeeker/JobDetails';
+import SavedJobs from './pages/JobSeeker/SavedJobs';
+import UserProfile from './pages/JobSeeker/UserProfile';
+import EmployerDashboard from './pages/Employer/EmployerDashboard';
+import ProtectRoute from './routes/ProtectRoute';
+import JobPostingForm from './pages/Employer/JobPostingForm';
+import ManageJobs from './pages/Employer/ManageJobs';
+import ApplicationViewer from './pages/Employer/ApplicationViewer';
+import EmployerProfilePage from './pages/Employer/EmployerProfilePage';
+
+
 function App() {
  
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div>
+
+      <Router>
+        <Routes>
+          {/*Public Routes */}
+          <Route path='/' element = {<LandingPage/>}/>
+          <Route path='/signup' element = {<SignUp/>}/>
+          <Route path='/login' element = {<Login/>}/>
+
+          <Route path='/find-jobs' element = {<JobSeekerDashboard/>}/>
+          <Route path='/job/:jobId' element = {<JobDetails/>}/>
+          <Route path='/saved-jobs' element = {<SavedJobs/>}/>
+          <Route path='/profile' element = {<UserProfile/>}/>
+
+          {/*Portect Routes */}
+          <Route element = {<ProtectRoute requiredRole = "employer" />}>
+            <Route path='/employer-dashboard' element = {<EmployerDashboard/>}/>
+            <Route path='/post-job' element = {<JobPostingForm/>}/>
+            <Route path='/manage-jobs' element = {<ManageJobs/>}/>
+            <Route path='/applicants' element = {<ApplicationViewer/>}/>
+            <Route path='/company-profile' element = {<EmployerProfilePage/>}/>
+          </Route>
+
+          {/*Cathe all route */}
+          <Route path='*' element = {<Navigate to="/" replace/>}/>
+        </Routes>
+      </Router>
+      <Toaster 
+        toastOptions={{
+        className : "",
+        style : {
+          fontSize : "13px"
+        }
+      }}
+      />
+    </div>
   )
 }
 

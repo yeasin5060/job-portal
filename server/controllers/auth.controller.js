a33750e7e3b5b838049deaf732085589aad3de06
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-08-14 10:03:23 +0600

    complete the login controller

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -14,7 +14,7 @@ export const register = async (req , res) => {
         const userExists = await User.findOne({email});
 
         if(userExists) {
-            return res.status(401).json({message : "User already exists"})
+            return res.status(401).json({message : "User already exists"});
         }
 
         const user = await User.create({
@@ -41,7 +41,26 @@ export const register = async (req , res) => {
 //@desc Login user
 export const login = async (req , res) => {
     try {
-        
+        const {email , password} = req.body;
+        const user = await User.findOne({email});
+
+        if(!user || !(await user.matchPassword(password))) {
+            return res.status(401).json({message : "Invalid email or password"});
+        }
+
+        res.status(200).json( {
+            _id : user._id,
+            name : user.name,
+            email : user.email,
+            avatar : user.avatar || '',
+            role : user.role,
+            token : generateToken(user._id),
+            companyName  : user.companyName || '',
+            companyDescription : user.companyDescription || '',
+            companyLogo : user.companyLogo || '',
+            resume : user.resume || ''
+        });
+
     } catch (error) {
         res.status(500).json({message : error.message});
     }

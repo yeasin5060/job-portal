fe5bfaab7cef9be67e2e0af9f5c589f081b674aa
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-08-14 09:33:54 +0600

    complete the protect middleware

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,25 +1,35 @@
+import { User } from "../models/user.model.js";
+import jwt from 'jsonwebtoken';
 
+// generate token
 
+const generateToken =  (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {expiresIn: '30d'});
+}
+ 
+//@desc Register new user
 export const register = async (req , res) => {
     try {
         
     } catch (error) {
-        
+        res.status(500).json({message : error.message});
     }
 }
 
+//@desc Login user
 export const login = async (req , res) => {
     try {
         
     } catch (error) {
-        
+        res.status(500).json({message : error.message});
     }
 }
 
+//@desc Get logged-in user
 export const getMe = async (req , res) => {
     try {
         
     } catch (error) {
-        
+        res.status(500).json({message : error.message});
     }
 }
--- server/middleware/auth.middleware.js
+++ server/middleware/auth.middleware.js
@@ -1,9 +1,19 @@
-
+import { User } from '../models/user.model.js';
+import jwt from 'jsonwebtoken';
 
 export const protect = async (req , res , next) => {
     try {
-        
+        let token = req.headers.authorization;
+
+        if(token && token.startsWith('Bearer')) {
+            token = token.split(' ')[1];
+            const decoded = jwt.verify(token ,process.env.JWT_SECRET);
+            req.user = await User.findById(decoded.id).select('-password');
+            next();
+        }else {
+            res.status(400).json({message : "Not authorized , no token"});
+        }
     } catch (error) {
-        
+        res.status(401).json({message : 'Token Filed' , error : error.message});
     }
 }

fd9cb23db67307b8af52a5061c516c43f25f4ad9
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-08-14 09:55:02 +0600

    complete the register controller

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -10,7 +10,29 @@ const generateToken =  (id) => {
 //@desc Register new user
 export const register = async (req , res) => {
     try {
-        
+        const {name , email , password , avatar , role} = req.body;
+        const userExists = await User.findOne({email});
+
+        if(userExists) {
+            return res.status(401).json({message : "User already exists"})
+        }
+
+        const user = await User.create({
+            name , email , password , avatar , role
+        });
+
+        res.status(200).json( {
+            _id : user._id,
+            name : user.name,
+            email : user.email,
+            avatar : user.avatar,
+            role : user.role,
+            token : generateToken(user._id),
+            companyName  : user.companyName || '',
+            companyDescription : user.companyDescription || '',
+            companyLogo : user.companyLogo || '',
+            resume : user.resume || ''
+        });
     } catch (error) {
         res.status(500).json({message : error.message});
     }

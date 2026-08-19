7bd3e26f661b20545d944ddbb7a5cc4df3ce71b4
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-08-19 09:16:35 +0600

    complete the job create comtroller

--- server/controllers/job.controller.js
+++ server/controllers/job.controller.js
@@ -1,10 +1,20 @@
+import { Application } from "../models/application.model.js";
+import { Job } from "../models/job.model.js";
+import { SavedJob } from "../models/savedJob.model";
+import { User } from "../models/user.model.js";
 
-
+// Crate a new job  (Employer Only)
 export const createJob = async (req , res) => {
     try {
-        
+        if(req.user.role !== 'employer') {
+            return res.status(403).json({message : "Only employers can job post"});
+        }
+
+        const job = await Job.create({...req.body, company : req.user._id});
+
+        res.status(201).json({ message : "Job create successfully" , job});
     } catch (error) {
-        
+        res.status(500).json({message : error.message});
     }
 }
 
@@ -13,7 +23,7 @@ export const getJobds = async (req , res) => {
     try {
         
     } catch (error) {
-        
+        res.status(500).json({message : error.message});
     }
 }
 
@@ -23,7 +33,7 @@ export const getJobById = async (req , res) => {
     try {
         
     } catch (error) {
-        
+        res.status(500).json({message : error.message});
     }
 }
 
@@ -32,7 +42,7 @@ export const updateJob = async (req , res) => {
     try {
         
     } catch (error) {
-        
+        res.status(500).json({message : error.message});
     }
 }
 
@@ -41,7 +51,7 @@ export const deleteJob = async (req , res) => {
     try {
         
     } catch (error) {
-        
+        res.status(500).json({message : error.message});
     }
 }
 
@@ -50,7 +60,7 @@ export const toggleCloseJob = async (req , res) => {
     try {
         
     } catch (error) {
-        
+        res.status(500).json({message : error.message});
     }
 }
 
@@ -58,7 +68,7 @@ export const getJobsEmployer = async (req , res) => {
     try {
         
     } catch (error) {
-        
+        res.status(500).json({message : error.message});
     }
 }
 

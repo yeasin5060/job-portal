2f6fd9cc82edbbb08af5f6004df865edd21264dc
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-08-19 15:00:20 +0600

    complete the getJobs controller

--- server/controllers/job.controller.js
+++ server/controllers/job.controller.js
@@ -20,8 +20,74 @@ export const createJob = async (req , res) => {
 
 
 export const getJobds = async (req , res) => {
+
+    const {
+        keyword,
+        location,
+        category,
+        type,
+        minSalary,
+        maxSalary,
+        userId
+    } = req.query;
+
+    const query = {
+        isClosed : false,
+        ...(keyword && {title : { $regex : keyword, $options : 'i' }}),
+        ...(location && {location : { $regex : location, $options : 'i' }}),
+        ...(keyword && {category }),
+        ...(type && { type }),
+    };
+
+    if(minSalary || maxSalary) {
+        query.$and = [];
+
+        if(minSalary) {
+            query.$and.push({salaryMax : {$gte : Number(minSalary)}});
+        }
+
+        if(maxSalary) {
+            query.$and.push({salaryMin : {$gte : Number(maxSalary)}});
+        }
+
+        if( query.$and.length === 0 ) {
+            delete query.$and;
+        }
+    }
     try {
-        
+        const jobs = await Job.find(query).populate(
+            'companyName',
+            'name companyName companyLogo'
+        );
+
+        let savedJobIds = [];
+        let appliedJobStatusMap = [];
+
+        if(userId) {
+            //saved job
+            const savedJobs = await SavedJob.find({jobseeker : userId}).select('job');
+            savedJobIds = savedJobs.map((s) => String(s.job));
+
+             //saved job
+            const applications = await Application.find({applicant : userId}).select('job status');
+            applications.forEach((app) => {
+                appliedJobStatusMap[String(app.job)] = app.status;
+            });
+        }
+
+
+        const jobsWithExtras = jobs.map((job)=> {
+            const jobIdStr = String(job._id);
+
+            return {
+                ...job.toObject(),
+                isSaved : savedJobIds.includes(jobIdStr),
+                applicationStatus : appliedJobStatusMap[jobIdStr] || null
+            };
+        });
+
+        res.json(jobsWithExtras);
+
     } catch (error) {
         res.status(500).json({message : error.message});
     }

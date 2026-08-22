44792c8ed2b0f02260a6d9c86c69c16bb75abdaf
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-08-22 09:19:37 +0600

    complete the applyToJob controller

--- server/controllers/application.controller.js
+++ server/controllers/application.controller.js
@@ -1,43 +1,64 @@
+import { Application } from "../models/application.model.js"
+import { Job } from "../models/job.model.js"
 
-
-export const applyToJob = async () => {
+    // apply to a job
+export const applyToJob = async (req, res) => {
     try {
-        
+        if(req.user.role !== "jobseeker") {
+            return res.satus(403).json({message : "Only job seeker can apply"});
+        }
+
+        const existing = await Application.findOne({
+            job : req.params.jobId,
+            applicant : req.user._id,
+        });
+
+        if(existing) {
+            return res.status(403).json({mesage : "Already applied to this job"});
+        }
+
+        const application = await Application.create({
+            job : req.params.jobId,
+            applicant : req.user._id,
+            resume : req.user.resume
+        });
+
+        res.status(201).json(application);
     } catch (error) {
-        
+        res.status(500).json({message : error.message});
     }
 }
 
 
-export const getMyApplications = async () => {
+export const getMyApplications = async (req, res) => {
     try {
         
     } catch (error) {
-        
+        res.status(500).json({message : error.message});
     }
 }
 
 
-export const getApplicantForJob = async () => {
+export const getApplicantForJob = async (req, res) => {
     try {
         
     } catch (error) {
-        
+       res.status(500).json({message : error.message});  
     }
 }
 
-export const getApplicationById = async () => {
+export const getApplicationById = async (req, res) => {
     try {
         
     } catch (error) {
-        
+        res.status(500).json({message : error.message});
     }
 }
 
-export const updateStatus = async () => {
+export const updateStatus = async (req, res) => {
     try {
         
     } catch (error) {
-        
+        res.status(500).json({message : error.message}); 
     }
 }

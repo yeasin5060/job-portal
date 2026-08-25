0a4947b235486e6b98db3ff2913e278d6c65298b
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-08-25 11:24:32 +0600

    setup the authcontext .jsx file

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import {createContext, useContext , useState , useEffect} from 'react'
 
-const AuthContext = () => {
-  return (
-    <div>AuthContext</div>
-  )
+const AuthContext = createContext();
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+
+  if(!context) {
+    throw new Error("useAuth must be use within an AuthProvider");
+  }
 }
 
-export default AuthContext
+
+export const AuthProvider = ({children}) => {
+  const [user , setUser] = useState(null);
+  const [loading , setLoding] = useState(true);
+  const [isAuthenticated , setIsAuthenticated] = useState(false);
+
+  useEffect (() => {
+    checkAuthStatus();
+  },[]);
+
+  const checkAuthStatus = async () => {};
+  const login = (userData , token) => {};
+  const logout = () => {};
+  const updateUser = (updateUserData) => {};
+
+  const value = {
+    user , loading , isAuthenticated , login , logout, updateUser, checkAuthStatus
+  }
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+};

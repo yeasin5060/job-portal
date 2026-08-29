962ca84e6e9af44b38067634a7ab177a8fda9560
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-08-29 10:30:15 +0600

    start the employerdashboard.jsx

---
+++ client/src/components/layout/DashboardLayout.jsx
@@ -0,0 +1,11 @@
+import {useEffect , useState} from 'react';
+import {Plus , Briefcase, Users , Building2, TrendingUp, CheckCircle2} from 'lucide-react';
+
+const DashboardLayout = ({activeMenu}) => {
+  return (
+    <div>DashboardLayout</div>
+  )
+}
+
+export default DashboardLayout
+
--- client/src/pages/Employer/EmployerDashboard.jsx
+++ client/src/pages/Employer/EmployerDashboard.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import {useEffect , useState} from 'react';
+import {Plus , Briefcase, Users , Building2, TrendingUp, CheckCircle2} from 'lucide-react';
+import moment from 'moment';
+import { useNavigate } from 'react-router-dom';
+import axiosInstance from '../../utils/axiosinstance';
+import { API_PATHS } from '../../utils/apiPaths';
+import DashboardLayout from '../../components/layout/DashboardLayout';
 
 const EmployerDashboard = () => {
+  const navigate = useNavigate();
+  const [dashboardData , setDashboardData] = useState(null);
+  const [isLoading , setIsLoading] = useState(false);
+
+  const getDashboardOverviwe = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axiosInstance.get(API_PATHS.DASHBOARD.OVERVIEW);
+      if(response.status === 200) {
+        setDashboardData(response.data);
+      }
+    } catch (error) {
+      console.log(error);
+    }finally {
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(()=> {
+    getDashboardOverviwe()
+  },[]);
   return (
-    <div>EmployerDashboard</div>
+    <DashboardLayout activeMenu="employer-dashboard">
+
+    </DashboardLayout>
   )
 }
 

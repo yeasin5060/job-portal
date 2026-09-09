55511bab0a5495795504e0f383804249bbe156da
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-09-09 15:43:19 +0600

    complete the filter section in managejobs.jsx

--- client/src/pages/Employer/ManageJobs.jsx
+++ client/src/pages/Employer/ManageJobs.jsx
@@ -11,7 +11,7 @@ const ManageJobs = () => {
 
   const navigate = useNavigate();
 
-  const [searchTerm , serSearchTerm] = useState("");
+  const [searchTerm , setSearchTerm] = useState("");
   const [statusFilter , setStatusFilter] = useState("All");
   const [currentPage , setCurrentPage] = useState(1);
   const [sortField , setSortField] = useState("title");
@@ -133,6 +133,31 @@ const ManageJobs = () => {
         </div>
 
         {/*Filters */}
+        <div className='bg-white/80 backdrop-blur-sm rounded-2xl shadow-xl shadow-black/5 border border-white/20 p-6 mb-8'>
+          <div className='flex flex-col sm:flex-row gap-4'>
+            {/*Search */}
+            <div className='flex-1 relative'>
+              <div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
+                <Search className='h-4 w-4 text-gray-400'/>
+              </div>
+              <input className='block w-full pl-10 pr-4 py-2 border border-gray-200 rounded-lg focus:ring-2 focus:ring-blue-500/20 focus:border-blue-500 outline-0 transition-all duration-200 bg-gray-50/50 placeholder-gray-400' type='text' placeholder='Search' value={searchTerm} onChange={(e)=> setSearchTerm(e.target.value)}/>
+            </div>
+            {/* Status Filters */}
+            <div className='sm:w-48'>
+              <select className='block w-full px-4 py-2 text-sm border border-gray-200 rounded-lg focus:ring-2 focus:ring-blue-500/20 focus:border-blue-500 outline-0 transition-all duration-200' value={statusFilter} onChange={(e)=> setStatusFilter(e.target.value)}>
+                <option value="All">All Status</option>
+                <option value="Active">Active</option>
+                <option value="Closed">Closed</option>
+              </select>
+            </div>
+          </div>
+          {/* Results Summery */}
+          <div className='my-4'>
+            <p className='text-sm text-gray-600'>
+              Showing {paginatedJobs.length} of {filterAndSortJobs.length}{" "} Jobs
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   </DashboardLayout>

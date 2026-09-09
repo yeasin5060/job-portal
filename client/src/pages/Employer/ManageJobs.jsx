f10817aab27c14555d569b1e33898262a20ea08d
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-09-09 11:51:57 +0600

    complete the getPostedJobs function

--- client/src/pages/Employer/ManageJobs.jsx
+++ client/src/pages/Employer/ManageJobs.jsx
@@ -49,8 +49,35 @@ const ManageJobs = () => {
   const LoadingRow = () => <></>;
 
   const getPostedJobs = async (disableLoder) => {
-    
+    setIsLoading(!disableLoder);
+    try {
+      const response = await axiosInstance.get(API_PATHS.JOBS.GET_JOBS_EMPLOYER);
+
+      if(response.status === 200 && response.data?.length > 0) {
+        const formattedJobs = response.data?.map((job) => ({
+          id: job._id,
+          title : job?.title,
+          company : job?.company?.name,
+          status : job?.isClosed ? "Closed" : "Active",
+          applicants : job?.applicationCount || 0,
+          datePosted : moment(job?.createdAt).format("DD-MM-YYYY"),
+          logo : job?.company?.companyLogo
+        }));
+
+        setJobs(formattedJobs)
+      }
+    } catch (error) {
+      if(error.message) {
+        console.error(error.response.data.message);
+      }else {
+        console.error("Error posting job , Please try again")
+      }
+    }finally {
+      setIsLoading(false)
+    }
   };
+
+  
 
   useEffect (() => {
     getPostedJobs();

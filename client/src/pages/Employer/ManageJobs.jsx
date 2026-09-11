8103565ca6de20f26bc2890b5177f1142b59e992
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-09-11 16:27:03 +0600

    complete the pagination section in managejobs.jsx

--- client/src/pages/Employer/ManageJobs.jsx
+++ client/src/pages/Employer/ManageJobs.jsx
@@ -317,6 +317,58 @@ const ManageJobs = () => {
               )
             }
           </div>
+
+          {/* Pagination */}
+          {
+            totalPages > 1 && (
+              <div className='mt-6 flex items-center justify-between'>
+                <div className='flex-1 flex justify-between sm:hidden'>
+                  <button className='relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 disabled:opacity-50 disabled:cursor-not-allowed' onClick={() => setCurrentPage(Math.max(1 , currentPage - 1 ))} disabled = {currentPage === 1 }>
+                    Previous
+                  </button>
+                  <button className='ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 disabled:opacity-50 disabled:cursor-not-allowed'  onClick={() => setCurrentPage(Math.min( totalPages, currentPage + 1 ))} disabled = {currentPage === totalPages }>
+                    Next
+                  </button>
+                </div>
+                <div className='hidden sm:flex-1 sm:flex sm:items-center sm:justify-between'>
+                  <div >
+                    <p className='text-sm text-gray-700 mr-3'>
+                      Showing {" "}
+                      <span className='font-medium'>{startIndex + 1 } </span> to {" "}
+                      <span className='font-medium'>
+                        {
+                          Math.min(
+                            startIndex + itemsPerPage,
+                            filterAndSortJobs.length
+                          )
+                        }
+                      </span> {" "}
+                      of {" "}  
+                      <span className='font-medium'>
+                        {filterAndSortJobs.length}
+                      </span> {" "}
+                      results
+                    </p>
+                  </div>
+                  <div>
+                    <nav className='relative z-10 inline-flex rounded-md shadow-sm -space-x-px'>
+                      <button className='relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50  disabled:opacity-50 disabled:cursor-not-allowed' onClick={() => setCurrentPage(Math.max(1 , currentPage - 1 ))} disabled = {currentPage === 1 }>
+                        Previous
+                      </button>
+                      {Array.from({length : totalPages} , (_,i) => i + 1).map((page)=> ( 
+                        <button key={page} onClick={() => setCurrentPage(page)} className={`relative inline-flex items-center px-4 py-2 border text-sm font-medium ${currentPage === page ? "z-10 bg-blue-50 border-blue-500 text-blue-600" : "bg-white border-gray-300 text-gray-500 hover:bg-gray-500"}`}>
+                          {page}
+                        </button>
+                      ))}
+                      <button className='relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50  disabled:opacity-50 disabled:cursor-not-allowed' onClick={() => setCurrentPage(Math.min( totalPages, currentPage + 1 ))} disabled = {currentPage === totalPages }>
+                        Next
+                      </button>
+                    </nav>
+                  </div>
+                </div>
+              </div>
+            )
+          }
         </div>
       </div>
     </div>
